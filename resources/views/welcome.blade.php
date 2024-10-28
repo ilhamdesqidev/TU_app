86276f92a7b5863633f623b1d20962e5c0938b97
ilhamdesqidev <ilhamdesqidev@gmail.com>
2024-10-28 14:21:35 +0700

    sidqi said

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,6 @@
 @extends ('main')
  <link rel="stylesheet" href="/asset/css/dashboard.css">
+ <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 @section ('content')
 <body>
 
@@ -49,6 +50,132 @@
         </div>
     </div>
 </div>
+
+<section id="gambar">
+    <div class="container">
+        <h3 class="text-center section-subheading">Jurusan</h3>
+        <h1 class="text-cent section-heading">Jurusan</h1>
+    </div>
+    <div class="container">
+        <!-- slider-start -->
+        <div class="swiper gambar-slider">
+            <div class="swiper-wraper">
+                <div class="swiper-slide gambar-slide">
+                    <img src="" alt="gambar">
+               </div>
+               <div class="gambar-slide-content">
+                <!-- <h1 class="jrs">PPLG</h1> -->
+                <div class="gambar-slide-content-bottom">
+                    <h2 class="jrss">
+                        PPLG
+                    </h2>
+                    <!-- <h3 class="jrrs"> -->
+                        <!-- <span>4.5</span> -->
+                        <!-- <div class="rating"> -->
+
+                        <!-- </div> -->
+                <!-- </h3> -->
+                </div>
+               </div>
+            <!-- slider-end -->
+             <!-- slider-start -->
+        <div class="swiper gambar-slider">
+            <div class="swiper-wraper">
+                <div class="swiper-slide gambar-slide">
+                    <img src="" alt="gambar">
+               </div>
+               <div class="gambar-slide-content">
+                <!-- <h1 class="jrs">PPLG</h1> -->
+                <div class="gambar-slide-content-bottom">
+                    <h2 class="jrss">
+                        TKJ
+                    </h2>
+                    <!-- <h3 class="jrrs"> -->
+                        <!-- <span>4.5</span> -->
+                        <!-- <div class="rating"> -->
+
+                        <!-- </div> -->
+                <!-- </h3> -->
+                </div>
+               </div>
+            <!-- slider-end -->
+             <!-- slider-start -->
+        <div class="swiper gambar-slider">
+            <div class="swiper-wraper">
+                <div class="swiper-slide gambar-slide">
+                    <img src="" alt="gambar">
+               </div>
+               <div class="gambar-slide-content">
+                <!-- <h1 class="jrs">PPLG</h1> -->
+                <div class="gambar-slide-content-bottom">
+                    <h2 class="jrss">
+                        Akuntasi
+                    </h2>
+                    <!-- <h3 class="jrrs"> -->
+                        <!-- <span>4.5</span> -->
+                        <!-- <div class="rating"> -->
+
+                        <!-- </div> -->
+                <!-- </h3> -->
+                </div>
+               </div>
+            <!-- slider-end -->
+             <!-- slider-start -->
+        <div class="swiper gambar-slider">
+            <div class="swiper-wraper">
+                <div class="swiper-slide gambar-slide">
+                    <img src="" alt="gambar">
+               </div>
+               <div class="gambar-slide-content">
+                <!-- <h1 class="jrs">PPLG</h1> -->
+                <div class="gambar-slide-content-bottom">
+                    <h2 class="jrss">
+                        Animasi
+                    </h2>
+                    <!-- <h3 class="jrrs"> -->
+                        <!-- <span>4.5</span> -->
+                        <!-- <div class="rating"> -->
+
+                        <!-- </div> -->
+                <!-- </h3> -->
+                </div>
+               </div>
+            <!-- slider-end -->
+             <!-- slider-start -->
+        <div class="swiper gambar-slider">
+            <div class="swiper-wraper">
+                <div class="swiper-slide gambar-slide">
+                    <img src="" alt="gambar">
+               </div>
+               <div class="gambar-slide-content">
+                <!-- <h1 class="jrs">PPLG</h1> -->
+                <div class="gambar-slide-content-bottom">
+                    <h2 class="jrss">
+                        PPLG
+                    </h2>
+                    <!-- <h3 class="jrrs"> -->
+                        <!-- <span>4.5</span> -->
+                        <!-- <div class="rating"> -->
+
+                        <!-- </div> -->
+                <!-- </h3> -->
+                </div>
+               </div>
+            <!-- slider-end -->
+            </div>
+
+            <div class="gambar-slider-control">
+                <div class="swiper-button-prev slider-arrow">
+                <i class='bx bxs-chevron-right'></i>
+                </div>
+                <div class="swiper-button-prev slider-arrow">
+                <i class='bx bxs-chevron-left'></i>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</section>
 
 </body>
 </html>
