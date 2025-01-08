e302e6fe9ddda0a83afafc215f8db83b8a914d6a
ilhamdesqidev <ilhamdesqidev@gmail.com>
2025-01-08 21:59:41 +0700

    masih berantakan, tapi udh ganti sidebar

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -10,7 +10,7 @@
 <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <title>Tata Usaha</title>
 </head>
-<body>
+<!-- <body>
     <nav class="sidebar close">
         <header>
             <div class="image-text">
@@ -67,10 +67,29 @@
 
     
 
-<script src="/asset/js/main.js"></script>
+<script src="/asset/js/main.js"></script> -->
 
+<body id="body-pd">
+    <header class="header" id="header">
+        <div class="header_toggle"> <i class='bx bx-menu' id="header-toggle"></i> </div>
+        <div class="header_img"> <img src="https://i.imgur.com/hczKIze.jpg" alt=""> </div>
+    </header>
+    <div class="l-navbar" id="nav-bar">
+        <nav class="nav">
+            <div> 
+                <a href="#" class="nav_logo"> <i class='bx bx-layer nav_logo-icon'></i> <span class="nav_logo-name">BBBootstrap</span> </a>
+                <div class="nav_list">
+                    <a href="{{url('/')}}" class="nav_link active"> <i class='bx bx-grid-alt nav_icon'></i> <span class="nav_name">Dashboard</span> </a>
+                    <a href="{{url('klapper')}}" class="nav_link"> <i class='bx bx-user nav_icon'></i> <span class="nav_name">Users</span> </a> 
+                    <a href="#" class="nav_link"> <i class='bx bx-message-square-detail nav_icon'></i> <span class="nav_name">Messages</span> </a> 
+                    <a href="#" class="nav_link"> <i class='bx bx-bookmark nav_icon'></i> <span class="nav_name">Bookmark</span> </a> 
+                    <a href="#" class="nav_link"> <i class='bx bx-folder nav_icon'></i> <span class="nav_name">Files</span> </a> 
+                    <a href="#" class="nav_link"> <i class='bx bx-bar-chart-alt-2 nav_icon'></i> <span class="nav_name">Stats</span> </a> </div>
+            </div> <a href="#" class="nav_link"> <i class='bx bx-log-out nav_icon'></i> <span class="nav_name">SignOut</span> </a>
+        </nav>
+    </div>
 </body>
 @yield('content')
 </html>
 
-link template sidebar : https://bbbootstrap.com/snippets/bootstrap-5-sidebar-menu-toggle-button-34132202
+<!-- link template sidebar : https://bbbootstrap.com/snippets/bootstrap-5-sidebar-menu-toggle-button-34132202 -->
