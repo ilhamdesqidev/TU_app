307302f7e551bb76cf52ffb85fa62419d1fc3531
k4yyn <kyllaanfezaa.22@gmail.com>
2025-01-09 14:49:37 +0700

    sidebar

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -5,6 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!--css-->
     <link rel="stylesheet" href="/asset/css/main.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js">
+    <link href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css">
 
     <!-- Boxicons CSS -->
 <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
@@ -63,11 +67,7 @@
         </div>
       
     </div>
-    </nav>
-
-    
-
-<script src="/asset/js/main.js"></script> -->
+    </nav>-->
 
 <body id="body-pd">
     <header class="header" id="header">
@@ -77,7 +77,13 @@
     <div class="l-navbar" id="nav-bar">
         <nav class="nav">
             <div> 
-                <a href="#" class="nav_logo"> <i class='bx bx-layer nav_logo-icon'></i> <span class="nav_logo-name">BBBootstrap</span> </a>
+                <a href="#" class="nav_logo">
+                <div class="header_toggle">
+                    <img src="public/asset/img/Ellipse 1.svg" alt="Menu Icon" id="header-toggle">
+                </div>
+                     <span class="nav_logo-name">Tata Usaha
+                     <small class="nav_logo-subtitle">SMK AMALIAH 1&2</small>
+                     </span> </a>
                 <div class="nav_list">
                     <a href="{{url('/')}}" class="nav_link active"> <i class='bx bx-grid-alt nav_icon'></i> <span class="nav_name">Dashboard</span> </a>
                     <a href="{{url('klapper')}}" class="nav_link"> <i class='bx bx-user nav_icon'></i> <span class="nav_name">Users</span> </a> 
@@ -88,8 +94,10 @@
             </div> <a href="#" class="nav_link"> <i class='bx bx-log-out nav_icon'></i> <span class="nav_name">SignOut</span> </a>
         </nav>
     </div>
+    <script src="/asset/js/main.js"></script>
 </body>
 @yield('content')
 </html>
 
 <!-- link template sidebar : https://bbbootstrap.com/snippets/bootstrap-5-sidebar-menu-toggle-button-34132202 -->
+<!-- https://www.canva.com/design/DAGbrsyVeYk/UqAp5jrRcFZN10YxBNcL7A/edit?utm_content=DAGbrsyVeYk&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton -->
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,5 @@
 @extends ('main')
- <link rel="stylesheet" href="/asset/css/dashboard.css">
+ <link rel="stylesheet" href="/asset/css/dashboard2.css">
  <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 @section ('content')
@@ -50,94 +50,6 @@
         </div>
     </div>
 </div>
-
-<section class="slider-container">
-    <div class="wrapper">
-        <div class="container">
-            <input type="radio" name="slide" id="c1" checked>
-            <label for="c1" class="slider">
-                <div class="row">
-                    <div class="icon">1</div>
-                    <div class="description">
-                        <h4>TJKT</h4>
-                    </div>
-                </div>
-            </label>
-            <input type="radio" name="slide" id="c2" >
-            <label for="c2" class="slider">
-                <div class="row">
-                    <div class="icon">2</div>
-                    <div class="description">
-                        <h4>ANIMASI</h4>
-                    </div>
-                </div>
-            </label>
-            <input type="radio" name="slide" id="c3" >
-            <label for="c3" class="slider">
-                <div class="row">
-                    <div class="icon">3</div>
-                    <div class="description">
-                        <h4>PPLG</h4>
-                    </div>
-                </div>
-            </label>
-            <input type="radio" name="slide" id="c4" >
-            <label for="c4" class="slider">
-                <div class="row">
-                    <div class="icon">4</div>
-                    <div class="description">
-                        <h4>DKV</h4>
-                    </div>
-                </div>
-            </label>
-            <input type="radio" name="slide" id="c5" >
-            <label for="c5" class="slider">
-                <div class="row">
-                    <div class="icon">5</div>
-                    <div class="description">
-                        <h4>MP</h4>
-                    </div>
-                </div>
-            </label>
-            <input type="radio" name="slide" id="c6" >
-            <label for="c6" class="slider">
-                <div class="row">
-                    <div class="icon">6</div>
-                    <div class="description">
-                        <h4>AKUNTANSI</h4>
-                    </div>
-                </div>
-            </label>
-            <input type="radio" name="slide" id="c7" >
-            <label for="c7" class="slider">
-                <div class="row">
-                    <div class="icon">7</div>
-                    <div class="description">
-                        <h4>LPS</h4>
-                    </div>
-                </div>
-            </label>
-            <input type="radio" name="slide" id="c8" >
-            <label for="c8" class="slider">
-                <div class="row">
-                    <div class="icon">8</div>
-                    <div class="description">
-                        <h4>TATA BUSANA</h4>
-                    </div>
-                </div>
-            </label>
-            <input type="radio" name="slide" id="c9" >
-            <label for="c9" class="slider">
-                <div class="row">
-                    <div class="icon">9</div>
-                    <div class="description">
-                        <h4>BISNIS RETAIL</h4>
-                    </div>
-                </div>
-            </label>
-        </div>
-    </div>
-</section>
 </section>
 
 <script>
