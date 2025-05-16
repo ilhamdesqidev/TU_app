655949a87e148c9c3b6f9c28b732ff9990c51733
ilham.desqi <ilhamdesqidev@gmail.com>
2025-05-16 11:29:59 +0700

    icon surat masuk

--- resources/views/superadmin/arsip/surat_masuk/index.blade.php
+++ resources/views/superadmin/arsip/surat_masuk/index.blade.php
@@ -588,6 +588,13 @@
     </div>
 </div>
 
+<!-- Bootstrap Icons CSS -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap-icons.css">
+
+<!-- Bootstrap JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+
+
 <!-- JavaScript untuk menangani fungsi-fungsi -->
 <script>
     document.addEventListener('DOMContentLoaded', function() {
