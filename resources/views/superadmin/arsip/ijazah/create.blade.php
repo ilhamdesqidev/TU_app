b034c784b35178de23240a86981271b276988965
ilham.desqi <ilhamdesqidev@gmail.com>
2025-05-18 16:56:52 +0700

    bisa masuk creat ijazah

--- resources/views/superadmin/arsip/ijazah/create.blade.php
+++ resources/views/superadmin/arsip/ijazah/create.blade.php
@@ -1,12 +1,9 @@
 @extends('main')
-
 @section('content')
 <h2>Tambah Ijazah - Angkatan {{ $angkatan->nama }}</h2>
-
 <form action="{{ route('ijazah.store') }}" method="POST" enctype="multipart/form-data">
     @csrf
     <input type="hidden" name="angkatan_id" value="{{ $angkatan->id }}">
-
     <div class="mb-3">
         <label for="klapper_id" class="form-label">Pilih Siswa</label>
         <select name="klapper_id" id="klapper_id" class="form-control" required>
@@ -16,23 +13,23 @@
             @endforeach
         </select>
     </div>
-
     <div class="mb-3">
         <label for="tahun_lulus" class="form-label">Tahun Lulus</label>
         <input type="text" name="tahun_lulus" id="tahun_lulus" class="form-control" placeholder="contoh: 2024" required>
     </div>
-
     <div class="mb-3">
         <label for="nomor_ijazah" class="form-label">Nomor Ijazah</label>
         <input type="text" name="nomor_ijazah" id="nomor_ijazah" class="form-control" required>
     </div>
-
     <div class="mb-3">
         <label for="file_ijazah" class="form-label">File Ijazah (PDF/JPG/PNG)</label>
         <input type="file" name="file_ijazah" id="file_ijazah" class="form-control">
     </div>
-
     <button type="submit" class="btn btn-primary">Simpan Ijazah</button>
-    <a href="{{ route('ijazah.perAngkatan', $angkatan->klapper->id) }}" class="btn btn-secondary">Batal</a>
+    @if(isset($klapperId))
+        <a href="{{ route('ijazah.perAngkatan', ['klapper' => $klapperId]) }}" class="btn btn-secondary">Batal</a>
+    @else
+        <a href="{{ url()->previous() }}" class="btn btn-secondary">Batal</a>
+    @endif
 </form>
 @endsection
